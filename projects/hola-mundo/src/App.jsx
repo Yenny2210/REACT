dedcdcfeb6629bf18d96e867cee3056dd9ba9130
Yenny Durango <yennycarolinadurango@gmail.com>
2024-02-09 07:56:06 -0500

    actualizaciones

--- projects/hola-mundo/src/App.jsx
+++ projects/hola-mundo/src/App.jsx
@@ -1,15 +1,40 @@
 import React from 'react';
 import './App.css';
 import { TwitterFollowCard } from './TwitterFollowCard';
-export function App() {
-    const addAt = (userName)
-    return (
-        <React.Fragment>
-            <TwitterFollowCard isFollowing userName="yennydurango" name="Yenny Durango" />
-            <TwitterFollowCard isFollowing={false} userName="impact" name="Impacto" />
-            <TwitterFollowCard isFollowing userName="caracol" name="Caracol" />
-            <TwitterFollowCard isFollowing userName="minuto30" name="Minuto 30" />
-        </React.Fragment>
 
+
+export function App() {
+    const formattedUserName = <span>@ola</span>
+
+    return (
+        <section className='App'>
+            <TwitterFollowCard
+                formattedUserName={formattedUserName}
+                isFollowing
+                userName="yennydurango"
+                name="Yenny Durango"
+            />
+
+            <TwitterFollowCard
+                formattedUserName={formattedUserName}
+                isFollowing={false}
+                userName="impact"
+                name="Impacto"
+            />
+
+            <TwitterFollowCard
+                formattedUserName={formattedUserName}
+                isFollowing
+                userName="caracol"
+                name="Caracol"
+            />
+
+            <TwitterFollowCard
+                formattedUserName={formattedUserName}
+                isFollowing
+                userName="minuto30"
+                name="Minuto 30"
+            />
+        </section>
     )
 }
--- projects/hola-mundo/src/TwitterFollowCard.jsx
+++ projects/hola-mundo/src/TwitterFollowCard.jsx
@@ -1,14 +1,19 @@
 
-export function TwitterFollowCard({userName, name, isFollowing}) {
-    const imageSrc = `https://unavatar.io/${userName}`
+export function TwitterFollowCard({ formattedUserName, userName, name, isFollowing }) {
 
     return (
         <article className='tw-followCard'>
             <header className='tw-followCard-header'>
-                <img className='tw-followCard-avatar' src={imageSrc} alt="Avatar persona}" />
+                <img className='tw-followCard-avatar'
+                    src={`https://unavatar.io/${userName}`}
+                    alt="Avatar persona}"
+                />
+
                 <div className='tw-followCard-info'>
                     <strong>{name}</strong>
-                    <span>@{userName}</span>
+                    <span>
+                        {formattedUserName}
+                    </span>
                 </div>
             </header>
 
